fe977a0cbe8ff274a00f3160081a79632a4352f8
chris1085 <itscfchiang@gmail.com>
2020-09-03 14:36:39 +0800

    feat: plot page finished. sending System finished. Main ongoing

---
+++ TMSSmail.js
@@ -0,0 +1,233 @@
+#!/usr/bin/env node
+
+/**
+ * Sending email if we have some data status processed longer than expected.
+ * Setting nodemailer module and sending info.
+ *
+ * @param {*} content html content for nodemailer's html(key)
+ * @param {*} productType product of report-tracking system
+ * @param {*} flag to determin whether table content is empty. If it's true, it represents that data details need to send mail to related person.
+ * @returns
+ */
+function sendMail(content, flag, mailListFlag, groupMail) {
+  if (flag == false) {
+    return;
+  }
+  const subjectTitle = "TMSS Notify";
+  //引用 nodemailer
+  const nodemailer = require("nodemailer");
+
+  let sendList = [
+    "tklin@sofivagenomics.com.tw",
+    "yichu@sofivagenomics.com.tw",
+    "tsuling@sofivagenomics.com.tw",
+    "tehyang.hwa@sofivagenomics.com.tw",
+    "bmi.cfc@sofivagenomics.com.tw"
+  ];
+
+  if (mailListFlag === "secondary") {
+    sendList = groupMail;
+  }
+  //宣告發信物件
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    secure: false, // upgrade later with STARTTLS
+    auth: {
+      user: "bmi@sofivagenomics.com.tw",
+      pass: "sofiva6615"
+    }
+    // service: 'Gmail',
+    // auth: {
+    //   user: 'sofiva.bmi@gmail.com',
+    //   pass: 'zabvtgxrhahvjjqh'
+    // }
+  });
+
+  let options = {
+    //寄件者
+    from: "sofiva.bmi@gmail.com",
+    //收件者
+    to: sendList,
+    //副本
+    // cc: 'bmi.cfc@sofivagenomics.com.tw',
+    //密件副本
+    bcc: "bmi.cfc@sofivagenomics.com.tw",
+    //主旨
+    subject: subjectTitle, // Subject line
+    //純文字
+    text: "", // plaintext body
+    //嵌入 html 的內文
+    html: content
+  };
+
+  //發送信件方法
+  transporter.sendMail(options, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("訊息發送: " + info.response);
+    }
+  });
+}
+
+/**
+ *  Subject: To write total mail content in a variable
+ *  Method: Useing += to contiue the html content string. If flag is
+ *          true represents uncompleted data exists and need to send
+ *          to related person. we also have template like ./test.html
+ *          to test the mail content style is what we want.
+ *
+ * @param {*} data
+ * @returns
+ */
+function writeMailContent(data, mailListFlag, groupMail) {
+  // console.log(data);
+  const tempUrl = "http://192.168.116.232/TMSS2/#/plot";
+  let flag = 0;
+  flag = data.length > 0 ? 1 : 0;
+  let content = `
+    <h2>TMSS Notify</h2>
+    <p>The average temperature exceeded the threshold. Click<a href="${tempUrl}"> here</a> to check status.</p>
+    <p>The information is summerized below:</p>
+    <table style = "border-collapse: collapse; width:800px;">
+      <col width = "15%">
+      <col width = "10%">
+      <col width = "10%">
+      <col width = "10%">
+      <col width = "10%">
+      <col width = "10%">
+      <col width = "15%">
+      <col width = "20%">
+
+        <tr>
+          <th style = "border-bottom: 1px solid #ddd; padding:10px 0px; background-color: #6bc541;"> RefID </th>
+          <th style = "border-bottom: 1px solid #ddd; background-color: #6bc541;"> Floor </th>
+          <th style = "border-bottom: 1px solid #ddd; background-color: #6bc541;"> RefType </th>
+          <th style = "border-bottom: 1px solid #ddd; background-color: #6bc541;"> Accept Low </th>
+          <th style = "border-bottom: 1px solid #ddd; background-color: #6bc541;"> Avg Temp </th>
+          <th style = "border-bottom: 1px solid #ddd; background-color: #6bc541;"> Accept High </th>
+          <th style = "border-bottom: 1px solid #ddd; background-color: #6bc541;"> Owner </th>
+          <th style = "border-bottom: 1px solid #ddd; background-color: #6bc541;"> Event Time </th>
+        </tr>`;
+
+  for (let index = 0; index < data.length; index++) {
+    content += `<tr>
+            <td align='center' style='border-bottom: 1px solid #ddd; padding:10px 0px;'>${data[index].RefID}</td>
+            <td align='center' style='border-bottom: 1px solid #ddd;'>${data[index].Floor}</td>
+            <td align='center' style='border-bottom: 1px solid #ddd;'>${data[index].RefType}</td>
+            <td align='center' style='border-bottom: 1px solid #ddd;'>${data[index].Accept_lo}</td>
+            <td align='center' style='border-bottom: 1px solid #ddd;'>${data[index].AvgTemp}</td>
+            <td align='center' style='border-bottom: 1px solid #ddd;'>${data[index].Accept_hi}</td>
+            <td align='center' style='border-bottom: 1px solid #ddd;'>${data[index].Owner}</td>
+            <td align='center' style='border-bottom: 1px solid #ddd;'>${data[index].MidTime}</td>
+          </tr>`;
+  }
+
+  content += `\n</table>`;
+  content += `<div  style="padding-top: 100px; display:block">
+                  <br>
+                  <span>慧智基因</span>
+                  <br>
+                  <span>姜權芳 Chuan-Fang Chiang</span>
+                  <br>
+                  <span>生物資訊分析師</span>
+                  <br>
+                  <span>地址:台北市中正區寶慶路27號5樓</span>
+                  <br>
+                  <span>電話:(02)2382-6615 EXT.6503</span>
+            </div>`;
+  // console.log(data.el);
+  // console.log("Content" + content, flag);
+  sendMail(content, flag, mailListFlag, groupMail);
+  // console.log(data);
+  return content, flag;
+}
+
+const axios = require("axios");
+const cron = require("node-cron");
+
+// let alertCompareData = {};
+// let sampleRun;
+// const fs = require("fs");
+// const writeFile = (filename, content) => {
+//   fs.writeFile(filename, content, () => {});
+// };
+const date = new Date().toISOString().slice(0, 10);
+const url = "http://192.168.116.232/tms_rest/last30.php";
+// const recordPath = `/opt/lampp/htdocs/TMSS2/nodeJS/records${date}.txt`;
+// console.log(recordPath);
+cron.schedule("10,40 * * * *", function() {
+  let alertData = [];
+  let mailList = [];
+  axios
+    .get(`${url}`, { params: { Date: date } })
+    .then(res => {
+      alertData = [];
+      mailList = [];
+      const data = res.data;
+      data.forEach(el => {
+        console.log(el.NodeName, el.AvgTemp);
+        if (el.AvgTemp === "故障" || el.AvgTemp === "除霜") {
+          return true;
+        } else if (el.AvgTemp === "null") {
+          console.log("123");
+          alertData.push({
+            NodeName: el.NodeName,
+            RefID: el.RefID,
+            Floor: el.Floor,
+            RefType: el.RefType,
+            Accept_lo: el.Accept_lo,
+            AvgTemp: el.AvgTemp,
+            Accept_hi: el.Accept_hi,
+            Owner: el.Owner,
+            Email: el.Email,
+            MidTime: el.MidTime
+          });
+          mailList.push(el.Email);
+        } else if (
+          parseFloat(el.AvgTemp) < parseFloat(el.Accept_lo) ||
+          parseFloat(el.AvgTemp) > parseFloat(el.Accept_hi)
+        ) {
+          alertData.push({
+            NodeName: el.NodeName,
+            RefID: el.RefID,
+            Floor: el.Floor,
+            RefType: el.RefType,
+            Accept_lo: el.Accept_lo,
+            AvgTemp: el.AvgTemp,
+            Accept_hi: el.Accept_hi,
+            Owner: el.Owner,
+            Email: el.Email,
+            MidTime: el.MidTime
+          });
+          mailList.push(el.Email);
+        }
+      });
+      mailList = [...new Set(mailList)];
+      writeMailContent(alertData, "primary");
+
+      mailList.forEach(el => {
+        let tempAlert = [];
+        alertData.forEach(item => {
+          if (item.Email === el) {
+            tempAlert.push({
+              NodeName: item.NodeName,
+              RefID: item.RefID,
+              Floor: item.Floor,
+              RefType: item.RefType,
+              Accept_lo: item.Accept_lo,
+              AvgTemp: item.AvgTemp,
+              Accept_hi: item.Accept_hi,
+              Owner: item.Owner,
+              Email: item.Email,
+              MidTime: item.MidTime
+            });
+          }
+        });
+        writeMailContent(tempAlert, "secondary", el);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
--- src/views/Plot.vue
+++ src/views/Plot.vue
@@ -16,12 +16,120 @@
             class="nips-updatedTime"
             style="display: inline-flex;align-items:flex-end ;"
             >Last Updated:
-          </span> -->
+          </span>-->
         </div>
       </div>
     </div>
 
-    <div class="container justify-content-between d-flex">
+    <div class="container justify-content-between d-flex flex-wrap">
+      <div class="d-flex pt-4 pl-3 mb-3 flex-column menu">
+        <div class="d-flex align-items-center mb-3">
+          <span style="font-size: 18px" class="mr-3">溫度： </span>
+          <div class="btn-group btn-group-toggle mr-2" data-toggle="buttons">
+            <label
+              class="btn btn-outline-info mr-1"
+              id="tempAll"
+              :class="{ active: isCheckTempAll }"
+            >
+              <input
+                type="checkbox"
+                name="options"
+                @click="checkAll('temp')"
+                v-model="isCheckTempAll"
+              />
+              All
+            </label>
+            <label
+              class="btn btn-outline-info active"
+              v-for="(item, index) in filterInfo.temp"
+              :key="index"
+              :id="item"
+            >
+              <input
+                type="checkbox"
+                name="options"
+                :value="item"
+                @change="updateFilter('temp')"
+                v-model="filterInfo.tempTemp"
+              />
+              {{ item }}
+            </label>
+          </div>
+        </div>
+
+        <div class="d-flex align-items-center mb-3">
+          <span style="font-size: 18px" class="mr-3">樓層： </span>
+          <div class="btn-group btn-group-toggle mr-4" data-toggle="buttons">
+            <label
+              class="btn btn-outline-info mr-1"
+              id="floorAll"
+              :class="{ active: isCheckFloorAll }"
+            >
+              <input
+                type="checkbox"
+                name="options"
+                @click="checkAll('floor')"
+                v-model="isCheckFloorAll"
+              />All
+            </label>
+            <label
+              class="btn btn-outline-info active"
+              v-for="(item, index) in filterInfo.floor"
+              :key="index"
+              :id="item"
+            >
+              <input
+                type="checkbox"
+                name="options"
+                :value="item"
+                @change="updateFilter('floor')"
+                v-model="filterInfo.tempFloor"
+              />
+              <!-- <input type="checkbox" name="options" v-if="index !== 0" /> -->
+              {{ item }}
+            </label>
+          </div>
+        </div>
+
+        <div class="d-flex align-items-center mb-3">
+          <span style="font-size: 18px">負責人：</span>
+          <div class="btn-group btn-group-toggle mr-2" data-toggle="buttons">
+            <label
+              class="btn btn-outline-info mr-1"
+              id="ownerAll"
+              :class="{ active: isCheckOwnerAll }"
+            >
+              <input
+                type="checkbox"
+                name="options"
+                @click="checkAll('owner')"
+                v-model="isCheckOwnerAll"
+              />
+              All
+            </label>
+            <label
+              class="btn btn-outline-info active"
+              v-for="(item, index) in filterInfo.owner"
+              :key="index"
+              :id="item"
+            >
+              <input
+                type="checkbox"
+                name="options"
+                :value="item"
+                @change="updateFilter('owner')"
+                v-model="filterInfo.tempOwner"
+              />
+              {{ item }}
+            </label>
+          </div>
+        </div>
+
+        <div class="menu-title">
+          <span>Selector</span>
+        </div>
+      </div>
+
       <div>
         <span class="mr-3">日期：{{ tempDate }}</span>
         <date-picker
@@ -29,33 +137,31 @@
           type="date"
           :disabled-date="notAfterToday"
           placeholder="Select Day"
-          class=""
+          class
           value-type="format"
           format="YYYY-MM-DD"
           @change="selectDate"
         ></date-picker>
       </div>
       <div>
-        <a href="" @click.prevent="getData()">刷新</a>
+        <a href @click.prevent="getData()">刷新</a>
       </div>
     </div>
 
-    <div class="d-flex flex-wrap px-3  pb-5">
-      <!-- {{ tempNodeCat }} -->
+    <div class="d-flex flex-wrap px-3 pb-5">
       <div
         class="col-12 col-md-12 col-lg-6 col-xl-4 mt-5 px-5"
         v-for="(item, index) in chartOptions"
         :key="index"
       >
-        <div class="d-flex justify-content-between mb-2">
-          <span>樓層：</span>
-          <span>負責人：</span>
-          <span>冰箱溫度： ℃</span>
+        <div class="d-flex justify-content-between mb-2 flex-wrap">
+          <span class="col-4">樓層：{{ refInfo[index].floor }}</span>
+          <span class="col-4">負責人：{{ refInfo[index].owner }}</span>
+          <span class="col-4">溫度：{{ refInfo[index].refType }} ℃</span>
         </div>
         <highcharts
           :options="chartOptions[index]"
           :callback="myCallback"
-          class=""
         ></highcharts>
       </div>
     </div>
@@ -71,7 +177,7 @@ import Footer from "@/components/Footer.vue";
 import Nav from "@/components/Nav.vue";
 import DatePicker from "vue2-datepicker";
 import "vue2-datepicker/index.css";
-
+import $ from "jquery";
 import axios from "axios";
 axios.defaults.withCredentials = false;
 import "vue-loading-overlay/dist/vue-loading.css";
@@ -93,12 +199,24 @@ export default {
       isLoading: false,
       fullPage: true,
       posts: false,
+      isCheckTempAll: true,
+      isCheckOwnerAll: true,
+      isCheckFloorAll: true,
       tempData: [],
+      refInfo: [],
       tempNodes: [],
       uniqueNodes: [],
       dateChoosed: null,
       tempDate: "",
       timer: "",
+      filterInfo: {
+        floor: [],
+        owner: [],
+        temp: [],
+        tempFloor: [],
+        tempOwner: [],
+        tempTemp: []
+      },
       colorSet: {
         maxColor: "#FF0000",
         aveColor: "#00CC66",
@@ -120,27 +238,46 @@ export default {
     },
     getData(date = new Date().toISOString().slice(0, 10)) {
       const url = "http://192.168.116.232/tms_rest/halfHrsTemperature.php";
-      // console.log("123");
       this.isLoading = true;
-
       this.tempDate = date;
 
       axios
         .get(`${url}`, { params: { Date: date } })
         .then(res => {
           this.tempData = res.data;
-          console.log(res.data);
+          // console.log(res.data);
           let tempNodesName = [];
+          this.tempNodes = [];
+
           this.tempData.forEach(el => {
             tempNodesName.push(el.NodeName);
+            this.filterInfo.owner.push(el.Owner);
+            this.filterInfo.floor.push(el.Floor);
+            this.filterInfo.temp.push(el.RefType);
           });
+          this.filterInfo.owner = [...new Set(this.filterInfo.owner)];
+          this.filterInfo.floor = [...new Set(this.filterInfo.floor)];
+          this.filterInfo.temp = [...new Set(this.filterInfo.temp)];
+          this.filterInfo.floor = this.filterInfo.floor.sort((a, b) =>
+            a < b ? -1 : 1
+          );
+          this.filterInfo.temp = this.filterInfo.temp.sort((a, b) =>
+            a < b ? -1 : 1
+          );
+          this.filterInfo.tempOwner = this.filterInfo.owner;
+          this.filterInfo.tempFloor = this.filterInfo.floor;
+          this.filterInfo.tempTemp = this.filterInfo.temp;
 
           if (this.uniqueNodes.length === 0) {
             this.uniqueNodes = [...new Set(tempNodesName)];
           }
-          // console.log(this.uniqueNodes);
 
           this.uniqueNodes.forEach(el => {
+            // let acceptHi = 0;
+            // let acceptLow = 0;
+            let max = -10000;
+            let min = 10000;
+
             const filterNodes = this.tempData.filter(function(item) {
               return item.NodeName === el;
             });
@@ -155,6 +292,16 @@ export default {
               this.tempNodeTemp.max.push(el.MaxTemp);
               this.tempNodeTemp.min.push(el.MinTemp);
               this.tempNodeTemp.ave.push(el.AvgTemp);
+
+              max =
+                max < parseFloat(el.MaxTemp, 10)
+                  ? parseFloat(el.MaxTemp, 10)
+                  : max;
+
+              min =
+                min > parseFloat(el.MinTemp, 10)
+                  ? parseFloat(el.MinTemp, 10)
+                  : min;
             });
 
             this.tempNodeTemp.max = this.tempNodeTemp.max.map(function(x) {
@@ -167,12 +314,32 @@ export default {
               return parseFloat(x, 10);
             });
 
+            if (max > parseFloat(filterNodes[0].Accept_hi, 10)) {
+              max = max + 0.5;
+            } else {
+              max = parseFloat(filterNodes[0].Accept_hi, 10) + 0.5;
+            }
+
+            if (parseFloat(filterNodes[0].Accept_lo, 10) > min) {
+              min = min - 0.5;
+            } else {
+              min = parseFloat(filterNodes[0].Accept_lo, 10) - 0.5;
+            }
+
             this.tempNodes.push({
               nodeName: el,
               maxTemp: this.tempNodeTemp.max,
               minTemp: this.tempNodeTemp.min,
               aveTemp: this.tempNodeTemp.ave,
-              tempNodeCat: this.tempNodeCat
+              tempNodeCat: this.tempNodeCat,
+              owner: filterNodes[0].Owner,
+              id: filterNodes[0].RefID,
+              refType: filterNodes[0].RefType,
+              floor: filterNodes[0].Floor,
+              acceptHi: parseFloat(filterNodes[0].Accept_hi, 10),
+              acceptLow: parseFloat(filterNodes[0].Accept_lo, 10),
+              ymax: max,
+              ymin: min
             });
 
             this.tempNodeCat = [];
@@ -188,8 +355,6 @@ export default {
           this.isLoading = false;
 
           this.tempNodes.forEach(el => {
-            // console.log(el.maxTemp);
-            // el.maxTemp.push(null);
             while (el.maxTemp.length < 49) {
               el.maxTemp.push(null);
               el.minTemp.push(null);
@@ -197,18 +362,39 @@ export default {
             }
           });
 
-          console.log(this.tempNodes);
+          this.refInfo = this.tempNodes.sort((a, b) => (a.id < b.id ? -1 : 1));
 
-          this.render();
+          this.isCheckTempAll = true;
+          this.isCheckOwnerAll = true;
+          this.isCheckFloorAll = true;
+
+          this.filterInfo.temp.forEach(el => {
+            $("#" + el).addClass("active");
+          });
+          this.filterInfo.owner.forEach(el => {
+            $("#" + el).addClass("active");
+          });
+          this.filterInfo.floor.forEach(el => {
+            $("#" + el).addClass("active");
+          });
+
+          this.render(this.tempNodes);
         })
         .catch(err => {
           console.log(err);
         });
     },
-    render() {
+    render(data) {
+      this.chartOptions = [];
+
       const vm = this;
-      this.tempNodes.forEach(function(item, index) {
-        // console.log(item);
+      // console.log(data);
+      if (data.length === 0) {
+        vm.chartOptions = [];
+      }
+
+      data.forEach(function(item, index) {
+        console.log(item.id, item.ymax, item.ymin);
         // console.log(vm.chartOptions);
         vm.chartOptions[index] = {
           credits: {
@@ -230,7 +416,7 @@ export default {
             }
           },
           title: {
-            text: item.nodeName,
+            text: item.id,
             style: { fontSize: "24px", fontWeight: "bold" },
             margin: 30
           },
@@ -244,7 +430,32 @@ export default {
               margin: 25
             },
             labels: { style: { fontSize: "16px" } },
-            margin: 10
+            margin: 10,
+            plotLines: [
+              {
+                color: "#FF66B2",
+                width: 1.5,
+                value: item.acceptHi,
+                zIndex: 5,
+                label: {
+                  text: "允收上界",
+                  align: "right"
+                }
+              },
+              {
+                color: "#FF66B2",
+                width: 1.5,
+                value: item.acceptLow,
+                zIndex: 5,
+                label: {
+                  text: "允收下界",
+                  align: "right",
+                  y: 15
+                }
+              }
+            ],
+            min: item.ymin,
+            max: item.ymax
           },
           xAxis: {
             categories: item.tempNodeCat,
@@ -320,7 +531,7 @@ export default {
           }
         };
       });
-      this.tempNodes = [];
+
       this.tempData = [];
     },
     notAfterToday(date) {
@@ -332,6 +543,119 @@ export default {
     },
     cancelAutoUpdate() {
       clearInterval(this.timer);
+    },
+    checkAll(filterCat) {
+      let data = this.tempNodes;
+      if (filterCat === "temp") {
+        this.isCheckTempAll = !this.isCheckTempAll;
+        this.filterInfo.tempTemp = this.isCheckTempAll
+          ? this.filterInfo.temp
+          : [];
+        this.filterInfo.temp.forEach(el => {
+          if (this.isCheckTempAll) {
+            $("#" + el).addClass("active");
+          } else {
+            $("#" + el).removeClass("active");
+            data = [];
+          }
+        });
+      } else if (filterCat === "owner") {
+        this.isCheckOwnerAll = !this.isCheckOwnerAll;
+        this.filterInfo.tempOwner = this.isCheckOwnerAll
+          ? this.filterInfo.owner
+          : [];
+        this.filterInfo.owner.forEach(el => {
+          if (this.isCheckOwnerAll) {
+            $("#" + el).addClass("active");
+          } else {
+            $("#" + el).removeClass("active");
+            data = [];
+          }
+        });
+      } else if (filterCat === "floor") {
+        this.isCheckFloorAll = !this.isCheckFloorAll;
+        this.filterInfo.tempFloor = this.isCheckFloorAll
+          ? this.filterInfo.floor
+          : [];
+        this.filterInfo.floor.forEach(el => {
+          if (this.isCheckFloorAll) {
+            $("#" + el).addClass("active");
+          } else {
+            $("#" + el).removeClass("active");
+            data = [];
+          }
+        });
+      }
+
+      if (this.isCheckTempAll && this.isCheckFloorAll && this.isCheckOwnerAll) {
+        data = this.tempNodes;
+        this.refInfo = [];
+        data.forEach(item => {
+          this.refInfo.push({
+            floor: item.floor,
+            owner: item.owner,
+            refType: item.refType
+          });
+          // console.log(item, index, this.refInfo);
+        });
+      } else {
+        data = [];
+      }
+
+      this.render(data);
+    },
+    updateFilter(filterCat) {
+      let data = this.tempNodes;
+      if (filterCat === "temp") {
+        if (this.filterInfo.tempTemp.length === this.filterInfo.temp.length) {
+          $("#tempAll").addClass("active");
+          this.isCheckTempAll = true;
+        } else {
+          $("#tempAll").removeClass("active");
+          this.isCheckTempAll = false;
+        }
+      } else if (filterCat === "owner") {
+        if (this.filterInfo.tempOwner.length === this.filterInfo.owner.length) {
+          $("#ownerAll").addClass("active");
+          this.isCheckOwnerAll = true;
+        } else {
+          $("#ownerAll").removeClass("active");
+          this.isCheckOwnerAll = false;
+        }
+      } else if (filterCat === "floor") {
+        if (this.filterInfo.tempFloor.length === this.filterInfo.floor.length) {
+          $("#floorAll").addClass("active");
+          this.isCheckFloorAll = true;
+        } else {
+          $("#floorAll").removeClass("active");
+          this.isCheckFloorAll = false;
+        }
+      }
+      let tempData = data.filter(el => {
+        this.filterInfo.tempTemp.includes(el.refType);
+        return this.filterInfo.tempTemp.includes(el.refType);
+      });
+
+      tempData = tempData.filter(el => {
+        return this.filterInfo.tempOwner.includes(el.owner);
+      });
+
+      tempData = tempData.filter(el => {
+        return this.filterInfo.tempFloor.includes(el.floor);
+      });
+
+      this.refInfo = [];
+      tempData.forEach(item => {
+        this.refInfo.push({
+          floor: item.floor,
+          owner: item.owner,
+          refType: item.refType
+        });
+        // console.log(item, index, this.refInfo);
+      });
+
+      console.log(tempData);
+      this.render(tempData);
     }
   },
   mounted() {
@@ -347,6 +671,16 @@ export default {
 </script>
 
 <style>
+div,
+span,
+h1,
+h2,
+h3,
+h4,
+h5 {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI",
+    "Microsoft JhengHei", Roboto, "Helvetica Neue", Arial, sans-serif;
+}
 .highcharts-container {
   width: 600px;
   height: 400px;
